a97474cfb03205aa922a71448841666d7ef7d118
nidhisingh16 <nidhisinghwgs@gmail.com>
2023-12-12 11:20:39 +0530

    Added logic for Carousal

--- app/src/main/java/com/example/travelproj/MainActivity.kt
+++ app/src/main/java/com/example/travelproj/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.travelproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private var currImg = 0
+    private lateinit var image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.p0)
+
+        val next = findViewById<ImageButton>(R.id.frwd)
+        val prev = findViewById<ImageButton>(R.id.prev)
+
+        next.setOnClickListener {
+            updateImage(true)
+        }
+
+        prev.setOnClickListener {
+            updateImage(false)
+        }
     }
+
+    private fun updateImage(isNext: Boolean) {
+        image.alpha = 0f
+
+        if (isNext) {
+            currImg = (currImg + 1) % 5
+        } else {
+            currImg = (5 + currImg - 1) % 5
+        }
+
+        val idImageToShowString = "p$currImg"
+        val idImageToShowInt = resources.getIdentifier(idImageToShowString, "id", packageName)
+        image = findViewById(idImageToShowInt)
+        image.alpha = 1f
+    }
+
 }
